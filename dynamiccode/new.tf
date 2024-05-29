f37c3631685d0e551e2f4fb3fdb383657f332589
Anatoly Ostrovsky <anatolyostrovsky91@gmail.com>
2024-05-29 12:29:46 +0100

    Dynamic Security Group

---
+++ dynamiccode/new.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+resource "aws_security_group" "new_server_sg" {
+  name        = "Dynamic Security Group"
+  description = "web Server Security Group"
+
+
+
+
+  dynamic "ingress" {
+    for_each = ["80", "443", "8080", "1541", "9092"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
