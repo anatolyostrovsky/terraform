e799e7f3f257f705b5083e9c283f3b40d825ae58
Anatoly Ostrovsky <anatolyostrovsky91@gmail.com>
2024-05-29 17:28:24 +0100

    update

--- webserver/webserver.tf
+++ webserver/webserver.tf
@@ -12,6 +12,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+
+resource "aws_eip" "new-static-ip" {
+  instance = aws_instance.web_server.id
+}
+
 data "aws_ami" "latest-amazon-linux-image" {
   most_recent = true
   owners      = ["amazon"]
@@ -25,19 +31,13 @@ resource "aws_instance" "web_server" {
   ami                    = data.aws_ami.latest-amazon-linux-image.id # Latest Amazon Linux AMI
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.new_server_sg.id]
-  user_data              = <<EOF
-#!/bin/bash
-sudo yum -y update
-sudo yum -y install httpd
-myip='curl checkip.amazonaws.com'
-sudo usermod -a -G apache ec2-user
-sudo chown -R ec2-user:apache /var/www
-sudo chmod 2775 /var/www
-find /var/www -type f -exec sudo chmod 0664 {} \;
-sudo echo "<h2>WebServer with IP: $myip</h2> Made with Terraform" > /var/www/html/index.html
-sudo service httpd start
-chkconfig httpd on
-EOF
+  user_data              = file("user-data.sh")
+  lifecycle {
+    create_before_destroy = true
+  }
+
+
+
 }
 
 resource "aws_security_group" "new_server_sg" {
@@ -67,3 +67,5 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
+
+
